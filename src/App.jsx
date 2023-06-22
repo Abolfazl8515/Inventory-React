61663062455674e65473a70c6d227427496b5960
Abolfazl <boorbooryabolfazl85@gmail.com>
2023-06-22 18:03:37 +0330

    added tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import './App.css'
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Intentory</h1>
-    </div>
-  )
-}
+const App = () => {
+  return <div className="App"></div>;
+};
 
-export default App
+export default App;
