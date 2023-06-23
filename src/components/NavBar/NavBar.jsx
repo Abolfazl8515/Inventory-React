f63eceae039dff1ec10ee3612590bd8a3a922fee
Abolfazl <boorbooryabolfazl85@gmail.com>
2023-06-23 13:23:43 +0330

    show number of products in navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,11 @@
-const NavBar = () => {
+const NavBar = ({numOfProducts}) => {
   return (
     <header className="w-full p-5 flex justify-center items-center bg-slate-600">
       <h1 className="font-yekan font-bold text-white text-2xl ml-3">
         برنامه انبارداری
       </h1>
       <span className="w-7 h-7 pt-1 flex justify-center items-center border border-solid border-white text-white rounded-full font-yekan">
-        0
+        {numOfProducts}
       </span>
     </header>
   );
--- src/components/Parent/Parent.jsx
+++ src/components/Parent/Parent.jsx
@@ -6,9 +6,11 @@ import Filters from "../Filters/Filters";
 import { useState } from "react";
 
 const Parent = () => {
+    const [categories, setCategories] = useState([]);
+    const [products, setProducts] = useState([]);
   return (
     <main className="App">
-      <NavBar />
+      <NavBar numOfProducts={products.length} />
       <section className="w-11/12 h-auto flex justify-between items-start">
         <div className="w-1/2 flex flex-col">
           <Filters />
